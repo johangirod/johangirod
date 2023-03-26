13607b3e3a5b85ab6d2383ed699e35b47dacfd05
Johan Girod <johan.girod@beta.gouv.fr>
2023-03-26 12:04:50 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<h1>Je suis Johan Girod</h1>
+
+<p>Je suis développeur indépendant pour l'administration, au sein du réseau beta.gouv.fr.</p>
+
+<p>
+	Depuis six ans, je porte <a href="https://mon-entreprise.fr">mon-entreprise.fr</a>, le site de
+	référence pour simuler les cotisations sociales et l'impôt sur le revenu des indépendants et
+	salariés.
+</p>
+
+<p>
+	J'ai participé à la création de plusieurs communs numériques, dont <a href="https://publi.codes"
+		>publi.codes</a
+	>, un langage déclaratif de modélisation de domaines complexe.
+</p>
+
+<p>
+	J'utilise l'écosystème <strong>JavaScript</strong>, en développant en
+	<strong>cycle itératifs courts</strong>, avec un focus sur la
+	<strong>performance, l'accessibilité, et l'experience usager</strong>.
+</p>
+
+<p>Basé à côté d'Albi, en télétravail.</p>
+
+<h2>Liens utiles</h2>
+<ul>
+	<a
+		href="https://github.com/johangirod/"
+		aria-label="Voir mes contributions sur Github, nouvelle fenêtre"
+		target="_blank">Voir mes contributions sur Github</a
+	>
+	<a
+		href="https://www.linkedin.com/in/johangirod/"
+		aria-label="Consulter mon CV sur Linkedin, nouvelle fenêtre"
+		target="_blank">Consulter mon CV sur Linkedin</a
+	>
+</ul>
