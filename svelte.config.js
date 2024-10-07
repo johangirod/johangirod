69c2da82b7b41ca2bfc983bdb18197edee8d9544
Johan Girod <dev@johangirod.com>
2024-10-07 15:35:11 +0200

    fix: increase max memory

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { escapeSvelte, mdsvex } from 'mdsvex';
 import shiki from 'shiki';
 
@@ -8,13 +8,12 @@ const mdsvexOptions = {
 	extensions: ['.md'],
 	highlight: {
 		highlighter: async (code, lang = 'text') => {
-			const highlighter = await shiki.getHighlighter({ theme: 'github-light' })
-			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
-			return `{@html \`${html}\` }`
+			const highlighter = await shiki.getHighlighter({ theme: 'github-light' });
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }));
+			return `{@html \`${html}\` }`;
 		}
-	},
-
-}
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -30,7 +29,6 @@ const config = {
 	},
 
 	extensions: ['.svelte', '.md']
-
 };
 
 export default config;
